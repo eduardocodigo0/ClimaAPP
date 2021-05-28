3571e0c45a7663698040d7c99e0a0b56521f4c83
eduardocodigo0 <eduardocodigo0@gmail.com>
2021-05-28 06:48:30 -0300

    Empty project

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    
+}
+rootProject.name = "ClimaAPP"
+
+
+include(":androidApp")
+include(":shared")
+
